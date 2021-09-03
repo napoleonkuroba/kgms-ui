95391a82fe7a4c1538690310f2ea44df31f2cbea
kuroba <napoleonkuroba@outlook.com>
2021-09-04 06:40:47 +0800

    配置代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy("/api", {
-      target: "napoleonxzy.cn:18080",
+      target: "http://napoleonxzy.cn:18080",
       changeOrigin: true,
       pathRewrite:{'^/api':''}
     })
